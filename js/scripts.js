ab13b2adc49ff687ae6878d0ecf561b286cf1a3e
Ben Parisot <me@benparisot.com>
2017-11-11 19:00:35 -0800

    include script to append each number in numList array as a list item to ul

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 //backend logic
-//var nums = [];
 
 
 //fronted logic
@@ -8,23 +7,66 @@ $(document).ready(function() {
 		event.preventDefault();
 		var numInput = parseInt($("#number").val());
 		var numList = [];
-		for (var index = 1; index <= numInput; index += 1) {
-			if (index % 15 === 0) {
-				numList.push("pingpong");
-			}
-			else if (index % 5 === 0) {
-				numList.push("pong");
-			}
-			else if (index % 3 === 0) {
-			 numList.push("ping");
-			}
+		for (var i =1; i <= numInput; i += 1) {
+			if (i % 15 === 0) {
+				var node = document.createElement("LI");
+				var textNode = document.createTextNode("pingpong");
+				node.appendChild(textNode);
+				document.getElementById("list").appendChild(node);
+
+				}
 			else {
-				numList.push(index);
+				var node = document.createElement("LI");
+				var textNode = document.createTextNode(i);
+				node.appendChild(textNode);
+				document.getElementById("list").appendChild(node);
 			}
-
-		};
-		var finalList = numList.join(" ");
-		console.log(finalList);
+		}
 	});
-
 });
+
+
+
+				// var listItem = document.createElement('li');
+				// var listItemVal = document.createTextNode("pingpong")
+			// 	// listItem.appendChild(document.createTextNode("pingpong"));
+			// 	// finaList.appendChild(listItem);
+			//
+			// 	// $("#list-item").text("pingpong");
+			// 	// numList.push("pingpong");
+			// }
+			// else if (index % 5 === 0) {
+			// 	var listItem = document.createElement('li');
+			// 	listItem.appendChild(document.createTextNode("pong"));
+			// 	finaList.appendChild(listItem);
+			//
+			// 	// $("#list-item").text("pong");
+			// 	// numList.push("pong");
+			// }
+			// else if (index % 3 === 0) {
+			// 	var listItem = document.createElement('li');
+			// 	listItem.appendChild(document.createTextNode("ping"));
+			// 	finaList.appendChild(listItem);
+			//
+			// 	// $("#list-item").text("ping");
+			//   // numList.push("ping");
+			// // }
+			// else {
+			// 	alert("no");
+				//$("#list-item").text(index);
+				// var listItem = document.createElement('li');
+				// listItem.appendChild(document.createTextNode(index));
+				// finaList.appendChild("hi");
+			// }
+
+
+
+
+
+		//$("#list-main").text(numList);
+		//var finalList = numList.join(" ");
+
+// 		console.log(numList);
+// 	};
+//
+// });
