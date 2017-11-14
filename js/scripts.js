ffcff5fe0aa167efcaa39b41b521d55a23d8a17c
Guest User <Guest@Epicodus-9D.local>
2017-11-13 20:46:10 -0800

    move function to backend logic and call it from frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 //backend logic
-
+var counter = function(numInput) {
+	for (var i =1; i <= numInput; i += 1) {
+		if (i % 15 === 0) {
+			var node = document.createElement("LI");
+			var textNode = document.createTextNode("chomp chomp chomp");
+			node.appendChild(textNode);
+			document.getElementById("list").appendChild(node);
+			}
+		else if (i % 5 === 0) {
+			var node = document.createElement("LI");
+			var textNode = document.createTextNode("nosh");
+			node.appendChild(textNode);
+			document.getElementById("list").appendChild(node);
+		}
+		else if (i % 3 === 0) {
+			var node = document.createElement("LI");
+			var textNode = document.createTextNode("nom");
+			node.appendChild(textNode);
+			document.getElementById("list").appendChild(node);
+		}
+		else {
+			var node = document.createElement("LI");
+			var textNode = document.createTextNode(i);
+			node.appendChild(textNode);
+			document.getElementById("list").appendChild(node);
+		}
+	}
+};
 
 //frontend logic
 $(document).ready(function() {
@@ -8,33 +35,9 @@ $(document).ready(function() {
 		$("#list").html("");
 		var numInput = parseInt($("#number").val());
 		if (isNaN(numInput)) {
-			alert("OKay, wiseguy. Try again - and enter a real number this time.");
-		}
-		for (var i =1; i <= numInput; i += 1) {
-			if (i % 15 === 0) {
-				var node = document.createElement("LI");
-				var textNode = document.createTextNode("chomp chomp chomp");
-				node.appendChild(textNode);
-				document.getElementById("list").appendChild(node);
-				}
-			else if (i % 5 === 0) {
-				var node = document.createElement("LI");
-				var textNode = document.createTextNode("nosh");
-				node.appendChild(textNode);
-				document.getElementById("list").appendChild(node);
-			}
-			else if (i % 3 === 0) {
-				var node = document.createElement("LI");
-				var textNode = document.createTextNode("nom");
-				node.appendChild(textNode);
-				document.getElementById("list").appendChild(node);
-			}
-			else {
-				var node = document.createElement("LI");
-				var textNode = document.createTextNode(i);
-				node.appendChild(textNode);
-				document.getElementById("list").appendChild(node);
-			}
+			alert("Okay, wiseguy. Try again - and enter a real number this time.");
+		} else {
+			counter(numInput);
 		}
 	});
 });
